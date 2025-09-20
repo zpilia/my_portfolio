cd50dd879bb78d7b36fa40601cb3c1b3c07dbc55
Zoé Pilia <61697702+zpilia@users.noreply.github.com>
2025-09-20 12:44:18 +0200

    [feature] Add dynamic current year in Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { FaInstagram, FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
     return (
         <footer className="border-t-2 border-gray-500">
             <div className="container mx-auto px-4 py-6 flex flex-col md:flex-row justify-between items-center">
-                <span className="text-sm text-white">© 2025 - All rights reserved</span>
+                <span className="text-sm text-white">© {currentYear} - All rights reserved</span>
 
                 <div className="flex space-x-6 mt-4 md:mt-0">
                     <a href="https://www.linkedin.com/in/zoé-pilia-1780161b0" className="text-white hover:text-[#0077B5]" target="_blank" rel="noopener noreferrer">
