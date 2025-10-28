eb0bf7d0c62c120900e326fce605fc31013a3cce
Zoé Pilia <61697702+zpilia@users.noreply.github.com>
2025-10-28 21:54:24 +0100

    [refactor/footer]: move social media links to texts.json for dynamic configuration

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,35 +1,18 @@
 import React from 'react';
 import { FaInstagram, FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa';
+import texts from '../../data/texts.json';
 
 const Footer = () => {
     const currentYear = new Date().getFullYear();
 
-    const iconData = [
-        {
-            name: 'LinkedIn',
-            icon: <FaLinkedin className="w-6 h-6" />,
-            href: 'https://www.linkedin.com/in/zoé-pilia-1780161b0',
-            hoverColor: '#0077B5'
-        },
-        {
-            name: 'GitHub',
-            icon: <FaGithub className="w-6 h-6" />,
-            href: 'https://github.com/zpilia',
-            hoverColor: '#9CA3AF'
-        },
-        {
-            name: 'Instagram',
-            icon: <FaInstagram className="w-6 h-6" />,
-            href: 'https://www.instagram.com/kimlai.2001/',
-            hoverColor: '#E4405F'
-        },
-        {
-            name: 'X (Twitter)',
-            icon: <FaTwitter className="w-6 h-6" />,
-            href: 'https://x.com/zoe_pilia',
-            hoverColor: '#1DA1F2'
-        }
-    ];
+    const { socials } = texts.footer;
+
+    const icons = {
+        LinkedIn: <FaLinkedin className="w-6 h-6" />,
+        GitHub: <FaGithub className="w-6 h-6" />,
+        Instagram: <FaInstagram className="w-6 h-6" />,
+        "X (Twitter)": <FaTwitter className="w-6 h-6" />
+    };
 
     return (
         <footer className="border-t-2 border-gray-500">
@@ -37,7 +20,7 @@ const Footer = () => {
                 <span className="text-sm text-white">© {currentYear} - All rights reserved</span>
 
                 <div className="flex space-x-6 mt-4 md:mt-0">
-                    {iconData.map(({ name, icon, href, hoverColor }) => (
+                    {socials.map(({ name, href, hoverColor }) => (
                         <div key={name} className="group relative inline-block">
                             <a
                                 href={href}
@@ -48,7 +31,7 @@ const Footer = () => {
                                 onMouseEnter={(e) => e.currentTarget.style.color = hoverColor}
                                 onMouseLeave={(e) => e.currentTarget.style.color = 'white'}
                             >
-                                {icon}
+                                {icons[name]}
                             </a>
                             <span
                                 className="absolute -top-10 left-1/2 transform -translate-x-1/2 z-20 px-2 py-1 text-sm font-bold text-white bg-gray-900 rounded-lg shadow-lg scale-0 group-hover:scale-100 transition-transform duration-300"
