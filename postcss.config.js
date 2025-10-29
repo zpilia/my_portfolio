f830e95b18799cb823f48f01daf3497abe386773
Zoé Pilia <61697702+zpilia@users.noreply.github.com>
2025-10-29 21:03:22 +0100

    [feature/ui]: customize page title and favicon

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     plugins: {
+        'postcss-preset-env': {
+            features: { 'nesting-rules': true },
+        },
         tailwindcss: {},
         autoprefixer: {},
     },
-}
+};
